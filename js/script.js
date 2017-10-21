ab21e48003778a501f765dbe7919ba7c89755045
Esayas Gebree <esayasgebree@gmail.com>
2017-10-21 13:19:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
+
 $( document ).ready(function() {
    $('#cancelApp').hide();
 });
 
-$('#newApp').click(function(){
+$('#newApp').click(function (){
    
    $('#ifError').empty();
    var value = $('#newApp').val();
 	if(value == 'NEW' ){
-		$('#newApp').attr({ type :'submit', value:'ADD' });
-		$('#addAppointment').show();
+		$('#newApp').attr({type :'submit', value:'ADD' });
+     		$('#addAppointment').show();
       		$('#cancelApp').show();
 	}else{
-		var inputDate = $('#vDate').val();
-		var valid = futureDate(inputDate);
-		
-      		if(valid === true){
-            		$("#appForm").submit();
-      		}else{
-         		$('#ifError').append("<strong>Error: Appointments shouldn't be made in the past.</strong>");
-      		}
+      		var date = $('#datepicker').val();
+      		var time = $('#time').val();
+     		var desc = $('#description').val();
+      
+      		if( date != '' && time != '' && desc != '' ){
+         		$("#appForm").submit();
+        
+     		}else{
+         		$('#ifError').append("<strong>Error: All inputs are requeired...</strong>");
+     		}
 	}
 });
 
+// To allow only future date
+$(function() {
+   $( "#datepicker" ).datepicker({minDate: 0});
+} );
 
-//to handle an appointments made in the past
-function futureDate(inputDate) {
-   var currentDate = new Date().getTime();
-   ddate = inputDate.split("-");
-   inputDate = new Date(ddate[0], ddate[1]-1, ddate[2]).getTime();
-   return (inputDate >= currentDate) ? true : false;
-}
    
 $('#cancelApp').click(function(){
    $('#newApp').attr({ type:'button', value:'NEW' });
@@ -37,42 +37,43 @@ $('#cancelApp').click(function(){
    $('#cancelApp').hide();
 });
 
+//$('#searchButton').click( function getAppointments(){
 $('#searchButton').click(getAppointments);
 function getAppointments(){
-
+  
    var search = $('#searchText').val();
    $.ajax({
-        type: 'POST',
-        url: '/cgi-bin/search.pl',
-        dataType: 'json',
-        data: { 'searchValue' : search },
-        success: function(data){
-            $('#tableContent').empty();
-            $('#ifNoAppData').empty();
-
-            if( data.length === 0 ){
-		$('#ifNoAppData').append('<h3>There is no appointment with description__:  <em>' + search + '</em>.</h3>');
-            }
-            else{
-               $('#tableContent').append('<tr><th>DATE</th> <th>TIME</th> <th>DESCRIPTION</th></tr>');
-               
-               for(var key in data){
-                  if(key){
-			var row = '<tr>';
-                  	row = row +  '<td>' + data[key].date + '</td>' +
-                     		'<td>' + data[key].time + '</td>' +
-                     		'<td>' + data[key].description + '</td>';
-                  	row = row + '</tr>';
-                  	$('#tableContent').append(row);
-                  }
-               } 
-            }
-          
-         },
-         error: function(){
-            alert("Handle Errors: Something is error in Ajax request.");
-        },
-        complete: function() {
-        }
-    });
+      type: 'POST',
+      url: '/cgi-bin/search.pl',
+      dataType: 'json',
+      data: { 'searchValue' : search },
+      success: function(data){
+         $('#tableContent').empty();
+         $('#ifNoAppData').empty();
+         
+         if( data.length === 0 ){
+            $('#ifNoAppData').append('<h3>There is no appointment with description__:  <em>' + search + '</em>.</h3>');
+         }
+         else{
+            $('#tableContent').append('<tr><th>DATE</th> <th>TIME</th> <th>DESCRIPTION</th></tr>');
+            
+            for(var key in data){
+               if(key){
+                  var row = '<tr>';
+                  row = row +  '<td>' + data[key].date + '</td>' +
+                     '<td>' + data[key].time + '</td>' +
+                     '<td>' + data[key].description + '</td>';
+                  row = row + '</tr>';
+                  
+                  $('#tableContent').append(row);
+               }
+            } 
+         }
+      },
+      error: function(){
+         alert("Handle Errors: something is error in Ajax request.");
+      },
+      complete: function() {
+      }
+  });
 }
