f0174767285e832495c9ac84becdfa219ead0472
Esayas Gebree <esayasgebree@gmail.com>
2017-10-22 15:31:32 -0500

    Update search.pl

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -3,33 +3,35 @@
 use DBI;
 use CGI qw(:standard);
 use utf8;
-use Encode;
 print header('application/json');
 use JSON -support_by_pp;
 
-#assign the serche value to the variable
-#called str that came from search inptbox 
-my $str =param("searchValue");
-my $dbh =DBI->connect("dbi:SQLite:appDB.db") || die "can not connect to the database";
-my $sql;
+#Assign the search value to the variable called str that we request from search inptbox 
+	my $str =param("searchValue");
+	
+	my $dbh =DBI->connect("dbi:SQLite:appDB.db")
+	|| die $DBI::errstr;
+	
+    	my $sql;
 
-#to get all appointment (if search input box null)
-if($str eq ""){
-	$sql='SELECT * FROM appointment';
-}
+#To get all appointments (if search input box null)
+	if($str eq ""){
+		$sql='SELECT * FROM appointment';
+	}
 
-#get recoreds only that have content of str in any location of the description attribute 
-$sql = 'SELECT * FROM appointment WHERE description LIKE ?';
-my $sth = $dbh->prepare($sql);
-$sth->execute("%$str%");
-my $data_json=[];
-while (my @row = $sth->fetchrow_array) {
-	#putting Result set to array of objcts
-	push @{$data_json},{'date'=>$row[1],'time'=>$row[2],'description'=>$row[3]}; 
-}
+#To get recoreds/appointments only that contain the value of str in any part of the description content  
+	$sql = 'SELECT * FROM appointment WHERE description LIKE ?';
+	my $sth = $dbh->prepare($sql);
+	$sth->execute("%$str%");
+	
+	my $data_json=[];
+	while (my @row = $sth->fetchrow_array) {
+	#putting the result set to the array of object
+		push @{$data_json},{'date'=>$row[1],'time'=>$row[2],'description'=>$row[3]}; 
+	}
+    	$sth->finish();
+    	$dbh -> disconnect();
 
-$dbh -> disconnect();
-
-# converting all pushed result sets of data_json to json object json_object
-my $json_object = to_json( $data_json, { utf8 => 1, pretty => 1 } ); 
-print "$json_object";
+#Converting all pushed result sets of data_json to json object json_object
+	my $json_object = to_json( $data_json, { utf8 => 1, pretty => 1 } ); 
+	print "$json_object";
