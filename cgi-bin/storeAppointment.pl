4d8aeb7e6ac9af6c8f86dad6a0844b0fa2d17383
Esayas Gebree <esayasgebree@gmail.com>
2017-10-22 15:08:33 -0500

    Update storeAppointment.pl

--- cgi-bin/storeAppointment.pl
+++ cgi-bin/storeAppointment.pl
@@ -1,39 +1,37 @@
 #!C:\xampp\perl/bin/perl.exe
 
-#DBI is the standard database interface module for Perl. It defines a set of methods, 
-#variables and conventions that provide a consistent database interface independent of the actual database being used.
 use DBI;
-use CGI qw(:standard -debug);
+use CGI qw(:standard);
 
-# checks post request
+#Checks post request
 if($ENV{'REQUEST_METHOD'} eq 'POST'){ 
 
-  my $dbh =DBI->connect("dbi:SQLite:appDB.db") || die "can not connect to the database";
-  
-  #assigning each parametres to the spacific variables and
-  #on bellow INSERT statment I have used it to store  to the database
-  my $date=param("date"); 
-  my $time=param("time"); 
-  my $descriptionValue=param("description"); 
+    my $dbh =DBI->connect("dbi:SQLite:appDB.db")
+    || die $DBI::errstr;
+
+#assigning each parametres to the spacific variables and
+#on bellow INSERT statment I have used it to store  to the database
+    my $date=param("date"); 
+    my $time=param("time"); 
+    my $descriptionValue=param("description"); 
    
-   #if appointment table not exist the bellow command
-   #will create a new table with the name of appointment. 
-  $dbh->do("CREATE TABLE appointment(
-           id INTEGER PRIMARY KEY,
+#if appointment table not exist the bellow command
+#will create a new table with the name of appointment. 
+    $dbh->do("CREATE TABLE appointment(
            date TEXT,
            time TEXT,
            description VARCHAR(100))");
-  
-#store the recored to the database table called appointment 
-  $dbh->do("INSERT INTO appointment(date,time,description) values(?,?,?)",
-		undef,
-		$date, $time,$descriptionValue);
-  
-#close database
-  $dbh->disconnect(); 
+
+#prepare statement and store the recored to the database table called appointment 
+    my $sth = $dbh->prepare("INSERT INTO appointment
+                          (date,time,description) VALUES(?,?,?)");
+    $sth ->execute($date, $time,$descriptionValue) || die $DBI::errstr;
+    $sth->finish();
+
+#clse database
+    $dbh->disconnect(); 
 }
 
 #Redirect to the main page (index.html in side directory perlApp)
 print ("Location: http://localhost\\appointmentApp\n");      
 print ("Content-type: text/html\n\n");
- 
